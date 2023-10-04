f950421e6ab01167cda3481f8ce5e4731dad69f7
Derek Marion <derek.j.marion@gmail.com>
2023-10-04 08:38:29 -0700

    First commit for working linearSearch function that passes all test cases

--- linearSearch.js
+++ linearSearch.js
@@ -1,8 +1,15 @@
 function linearSearch(searchTerm, arr) {
+  for (let element of arr) {//iterate through array and compare to search term
+    if (element === searchTerm) {
+      return arr.indexOf(element);
+    };
+  };
+  //return undefined if no match found
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
+  //use .match and global regex to build and array of indeces for matches
   return [];
 }
 
